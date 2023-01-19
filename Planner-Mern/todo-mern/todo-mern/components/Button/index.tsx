be82c9e5806dc0095f0765bbcb542a985a0dca8b
Jair <Jairjosafath@gmail.com>
2023-01-19 11:14:37 -0300

    making components

---
+++ Planner-Mern/todo-mern/todo-mern/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./button.styles.module.scss";
+import {
+  useRef,
+  useState,
+  useEffect,
+  RefObject,
+  MouseEvent,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "outline" | "text";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+}
+
+export default function Button({
+  variant = "primary",
+  size = "md",
+  ...props
+}: Props) {
+  const className = `${styles.content} ${styles[variant]} ${styles[size]}`;
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={className}>
+        <button {...props} />
+      </div>
+    </div>
+  );
+}
--- Planner-Mern/todo-mern/todo-mern/pages/_app.tsx
+++ Planner-Mern/todo-mern/todo-mern/pages/_app.tsx
@@ -1,12 +1,104 @@
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
-import "../styles/globals.css";
+import Button from "../components/Button";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
+      <div>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="primary"
+          size="xs"
+        >
+          Primary
+        </Button>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="outline"
+          size="xs"
+        >
+          Outline
+        </Button>
+        <Button onClick={() => console.log("clicked")} variant="text" size="xs">
+          Text
+        </Button>
+      </div>
+      <div>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="primary"
+          size="sm"
+        >
+          Primary
+        </Button>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="outline"
+          size="sm"
+        >
+          Outline
+        </Button>
+        <Button onClick={() => console.log("clicked")} variant="text" size="sm">
+          Text
+        </Button>
+      </div>
+      <div>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="primary"
+          size="md"
+        >
+          Primary
+        </Button>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="outline"
+          size="md"
+        >
+          Outline
+        </Button>
+        <Button onClick={() => console.log("clicked")} variant="text" size="md">
+          Text
+        </Button>
+      </div>
+      <div>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="primary"
+          size="lg"
+        >
+          Primary
+        </Button>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="outline"
+          size="lg"
+        >
+          Outline
+        </Button>
+        <Button onClick={() => console.log("clicked")} variant="text" size="lg">
+          Text
+        </Button>
+      </div>
+      <div>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="primary"
+          size="xl"
+        >
+          Primary
+        </Button>
+        <Button
+          onClick={() => console.log("clicked")}
+          variant="outline"
+          size="xl"
+        >
+          Outline
+        </Button>
+        <Button onClick={() => console.log("clicked")} variant="text" size="xl">
+          Text
+        </Button>
+      </div>
     </>
   );
 }
