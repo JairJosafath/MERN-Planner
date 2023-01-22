30dfb0439355b796929976aef457cbb2908780fa
Jair <Jairjosafath@gmail.com>
2023-01-22 01:18:55 -0300

    express with live mongo

--- Planner-Mern/backend/app.js
+++ Planner-Mern/backend/app.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const PORT = 3001;
-
-const cors = require("cors");
 const app = express();
-app.use(
-  cors({
-    origin: "http://127.0.0.1:4000",
-  })
-);
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 mongoose.set("strictQuery", false);
-// mongoose.connect(
-//   "mongodb+srv://todo-mern:ZYzKpBiM3gus3ryJ@cluster0.awmex.mongodb.net/?retryWrites=true&w=majority"
-// );
-mongoose.connect("mongodb://127.0.0.1:27017/todo-mern");
+mongoose.connect(
+  "mongodb+srv://todo-mern:ZYzKpBiM3gus3ryJ@cluster0.awmex.mongodb.net/?retryWrites=true&w=majority"
+);
+// mongoose.connect("mongodb://127.0.0.1:27017/todo-mern");
 const database = mongoose.connection.once("open", () =>
   console.log("connected to db")
 );
